238bc19a13e3a202c85cfa8dd09b635ab3eb0906
Fergal Moran <fergal.moran@gmail.com>
2022-11-08 15:42:11 +0000

    Extra API logging

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Gif } from '@models';
 import { GifContainer } from '@components';
+import { logger } from '@lib/logger';
 
 async function getGifs() {
+  logger.info('page', 'getGifs', `${process.env.API_URL}/api/gifs`);
   const gifs = await fetch(`${process.env.API_URL}/api/gifs`, {
     cache: 'no-store',
   });
+  logger.info('page', 'getGifs_gotGifs', gifs);
   return await gifs.json();
 }
 
